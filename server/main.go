bd0d50d2371e73d73b142389cef5f1337b0f033d
Samuel Boyden <sam@stboyden.com>
2023-10-17 00:52:56 +0100

    Handle SIGINT in TCP and UDP listeners

--- server/main.go
+++ server/main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
 	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -25,6 +27,7 @@ func makeParallel(functions ...func()) {
 func main() {
 	var tcpPortStr string
 	var udpPortStr string
+	gracefulCloseChannel := make(chan struct{})
 
 	if _p, isPresent := os.LookupEnv("TCP_PORT"); isPresent {
 		tcpPortStr = _p
@@ -54,16 +57,26 @@ func main() {
 		log.Fatal(":: SERVER :: [FATAL]: Could not start TCP socket listener: " + err.Error())
 	}
 	correctionSocketFunc := func() {
-		defer errorCorrectionSocket.Close()
+		log.Printf(":: SERVER :: [INFO]: Started error correction socket (TCP) on %d\n", tcpPort)
+		go func() {
+			<-gracefulCloseChannel
+			log.Println(":: SERVER - TCP :: [INFO]: Stopping...")
+			errorCorrectionSocket.Close()
+		}()
 
 		for {
 			conn, err := errorCorrectionSocket.Accept()
 
 			if err != nil {
-				log.Fatal(":: SERVER - TCP:: [FATAL]: Could not receive on TCP socket: " + err.Error())
+				if strings.Contains(err.Error(), "use of closed network connection") {
+					break
+				}
+
+				log.Println(":: SERVER - TCP :: [ERR]: Could not receive on TCP socket: " + err.Error())
+				continue
 			}
 
-			log.Printf(":: SERVER - TCP:: [INFO]: Connected with %s\n", conn.RemoteAddr())
+			log.Printf(":: SERVER - TCP :: [INFO]: Connected with %s\n", conn.RemoteAddr())
 
 			conn.Write([]byte("Hello!"))
 		}
@@ -76,19 +89,43 @@ func main() {
 		log.Fatal(":: SERVER :: [FATAL]: Could not start UDP socket listener: " + err.Error())
 	}
 	gameSocketFunc := func() {
-		defer gameSocket.Close()
+		log.Printf(":: SERVER :: [INFO]: Started game data socket (UDP) on %d\n", udpPort)
+		go func() {
+			<-gracefulCloseChannel
+			log.Println(":: SERVER - UDP :: [INFO]: Stopping...")
+			gameSocket.Close()
+		}()
 
 		buf := make([]byte, 2048)
 
 		for {
 			_, addr, err := gameSocket.ReadFrom(buf)
 			if err != nil {
-				log.Printf("::SERVER - UDP::  [WARN]: %s\n", err)
+				if strings.Contains(err.Error(), "use of closed network connection") {
+					break
+				}
+
+				log.Printf(":: SERVER - UDP :: [ERR]: %s\n", err)
+				continue
 			}
 
-			log.Printf(":: SERVER - UDP:: [INFO]: Received data from %s: %s\n", addr, string(buf))
+			log.Printf(":: SERVER - UDP :: [INFO]: Received data from %s: %s\n", addr, string(buf))
 		}
 	}
 
-	makeParallel(gameSocketFunc, correctionSocketFunc)
+	signalChannel := make(chan os.Signal, 2)
+	signal.Notify(signalChannel, os.Interrupt)
+	signalFunc := func() {
+		for s := range signalChannel {
+			if s == os.Interrupt {
+				gracefulCloseChannel <- struct{}{}
+				gracefulCloseChannel <- struct{}{}
+				log.Println(":: SERVER - SIGNAL HANDLER :: [INFO]: Received interrupt signal, gracefully shutting down")
+				break
+			}
+		}
+	}
+
+	makeParallel(gameSocketFunc, correctionSocketFunc, signalFunc)
+
 }
